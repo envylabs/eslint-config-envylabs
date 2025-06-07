ef8d79dc8fb95a4eb0b1581d26efe58bc71a7d44
Dray Lacy <dray@envylabs.com>
2025-06-07 12:53:12 -0400

    feat: add `eslint-plugin-import-x`
    This enables the "recommended" ruleset for eslint-plugin-import-x, and
    validates that imports are in a consistent order (and alphabetized).

--- configs/envylabs-js.js
+++ configs/envylabs-js.js
@@ -1,11 +1,28 @@
 import js from "@eslint/js";
+import { importX } from "eslint-plugin-import-x";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default [
   js.configs.recommended,
+  importX.flatConfigs.recommended,
   eslintPluginPrettierRecommended,
   {
     rules: {
+      "import-x/order": [
+        "warn",
+        {
+          alphabetize: { order: "asc" },
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            "parent",
+            "sibling",
+            "index",
+          ],
+          "newlines-between": "always",
+        },
+      ],
       "prettier/prettier": "warn",
     },
   },
--- configs/envylabs-ts.js
+++ configs/envylabs-ts.js
@@ -1,9 +1,12 @@
-import tseslint from "typescript-eslint";
+import { importX } from "eslint-plugin-import-x";
+import { configs as tseslintConfigs } from "typescript-eslint";
+
 import eslintPluginEnvylabs from "./envylabs-js.js";
 
 export default [
   ...eslintPluginEnvylabs,
-  ...tseslint.configs.recommended,
+  ...tseslintConfigs.recommended,
+  importX.flatConfigs.typescript,
   {
     files: ["**/*.ts", "**/*.tsx"],
     languageOptions: {
@@ -14,6 +17,9 @@ export default [
         projectService: true,
       },
       sourceType: "module",
+    },
+    rules: {
+      "@typescript-eslint/no-require-imports": "error",
     },
   },
   {
