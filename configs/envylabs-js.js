285d68c5699dc957d5cd02d127e7128614daad63
Dray Lacy <dray@envylabs.com>
2025-06-07 12:24:44 -0400

    feat: add format checking with prettier

--- configs/envylabs-js.js
+++ configs/envylabs-js.js
@@ -1,4 +1,12 @@
 import js from "@eslint/js";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
-export default [js.configs.recommended, eslintPluginPrettierRecommended];
+export default [
+  js.configs.recommended,
+  eslintPluginPrettierRecommended,
+  {
+    rules: {
+      "prettier/prettier": "warn",
+    },
+  },
+];
